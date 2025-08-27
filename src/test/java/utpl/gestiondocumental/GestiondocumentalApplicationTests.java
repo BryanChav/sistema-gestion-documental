0c56151ca49c2b92a4aa97ddada7f601f81b75b8
SuperUser <SuperUser@SuperUser.mshome.net>
2025-08-27 21:37:30 +0200

    Inicio

---
+++ src/test/java/utpl/gestiondocumental/GestiondocumentalApplicationTests.java
@@ -0,0 +1,13 @@
+package utpl.gestiondocumental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestiondocumentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
